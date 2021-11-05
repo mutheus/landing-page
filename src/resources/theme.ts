d8ee75ab4dfddef40a5f1c1f6803d64bee800463
Matheus <matheusalmeida016@gmail.com>
2021-11-05 07:00:48 -0400

    Add theme

---
+++ src/resources/theme.ts
@@ -0,0 +1,10 @@
+import { DefaultTheme } from 'styled-components'
+
+export const theme: DefaultTheme = {
+  colors: {
+    primary: '#252160',
+    secondary: '#3AFFE5',
+    white: '#FFF',
+    text: '#7B7B7B',
+  },
+}
