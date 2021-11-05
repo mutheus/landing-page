06127dffeeeabfb7ae46f169c3aad8cc53e5b9b9
Matheus <matheusalmeida016@gmail.com>
2021-11-05 06:17:07 -0400

    Add setup

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,7 @@
-function App () {
+export function App () {
   return (
     <>
-      <h1>CRA RTS-Template</h1>
-      <p>React + TypeScript + Styled Components</p>
+      <h1>Hello, World!</h1>
     </>
   )
 }
-
-export { App }
