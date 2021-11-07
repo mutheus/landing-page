0a47c18b1801124e95ae30683b0366f17ddf2664
Matheus <matheusalmeida016@gmail.com>
2021-11-07 09:07:08 -0400

    Add plan's card header

--- src/plans/plans.tsx
+++ src/plans/plans.tsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components/macro'
+import StarSvg from './assets/star-vector.svg'
 
 const PlansWrapper = styled.section`
   padding: 0 1em;
@@ -27,6 +28,59 @@ const Card = styled.div`
   border-radius: 10px;
 `
 
+type HeaderProps = {
+  star: string
+}
+
+const Header = styled.div<HeaderProps>`
+  display: flex;
+
+  div:first-child {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+
+    p {
+      font-size: .75rem;
+      color: #3C5882;
+      margin: 0;
+    }
+
+    h5 {
+      font-size: 1rem;
+      font-family: 'Nunito', sans-serif;
+      font-weight: 400;
+      margin: .313em 0;
+    }
+  }
+
+  div:last-child {
+    margin-left: auto;
+    color: ${({ theme }) => theme.colors.primary};
+    background-image: url(${({ star }) => star});
+    background-repeat: no-repeat;
+    background-size: contain;
+    padding: 2em;
+    transform: rotate(19.52deg);
+    text-align: center;
+    height: auto;
+    aspect-ratio: 1/1;
+    background-position: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+
+    h6 {
+      font-size: 1.25rem;
+      margin: 0;
+    }
+
+    span {
+      font-size: .5rem;
+    }
+  }
+`
+
 export function Plans () {
   return (
     <PlansWrapper>
@@ -35,11 +89,19 @@ export function Plans () {
       <Subtitle>Invista no seu futuro</Subtitle>
 
       <Card>
-        <div>
-          <p>1 ano de acesso</p>
+        <Header star={StarSvg}>
+          <div>
+            <p>1 ano de acesso</p>
 
-          <h5>Plano recomendado</h5>
-        </div>
+            <h5>Plano recomendado</h5>
+          </div>
+
+          <div>
+            <h6>70%</h6>
+
+            <span>de desconto</span>
+          </div>
+        </Header>
 
         <p>vantagens</p>
 
