4b3f7ed01da1d30f8b8049db3c3c3be80f137004
Matheus <matheusalmeida016@gmail.com>
2021-11-11 10:03:33 -0400

    Fix app screenshot

--- src/app-section/app-section.tsx
+++ src/app-section/app-section.tsx
@@ -1,45 +1,24 @@
 import RobotImg from 'shared/assets/robot-image.png'
-import styled from 'styled-components'
-import { Button } from 'shared/styles'
 import { ReactComponent as RightArrow } from 'shared/assets/right-arrow.svg'
 import PhoneImg from './assets/phone.png'
-
-const AppWrapper = styled.section`
-  padding: 3.375em 1em 0;
-  background-color: ${({ theme }) => theme.colors.primary};
-  display: flex;
-  flex-direction: column;
-
-  > img {
-    width: 100%;
-  }
-`
-
-const AppDetails = styled.div`
-  text-align: center;
-  max-width: 17.688em;
-  margin: 0 auto;
-  line-height: 1.5;
-
-  h3 {
-    color: ${({ theme }) => theme.colors.secondary};
-    font-size: .875rem;
-  }
-
-  p {
-    color: ${({ theme }) => theme.colors.white};
-    font-size: .75rem;
-  }
-`
-
-const AppButton = styled(Button)`
-  margin-top: 3em;
-`
+import { useEffect, useRef, useState } from 'react'
+import * as S from './styles'
 
 export function AppSection () {
+  const wrapperRef = useRef<HTMLImageElement>(null)
+  const [wrapperHeight, setWrapperHeight] = useState(0)
+
+  useEffect(() => {
+    window.addEventListener('resize', () => {
+      if (wrapperRef.current !== null) {
+        setWrapperHeight(wrapperRef.current?.getBoundingClientRect().height)
+      }
+    })
+  })
+
   return (
-    <AppWrapper>
-      <AppDetails>
+    <S.AppWrapper wrapperHeight={wrapperHeight}>
+      <S.AppDetails>
         <div>
           <img src={RobotImg} alt='Robot' />
 
@@ -48,10 +27,10 @@ export function AppSection () {
 
         <p>Receba lembretes no seu celular e tenha a facilidade de estudar a qualquer hora e de qualquer lugar.<br /> <strong>Baixe nosso app e confira.</strong></p>
 
-        <AppButton>Conheça nosso aplicativo <RightArrow /></AppButton>
-      </AppDetails>
+        <S.AppButton>Conheça nosso aplicativo <RightArrow /></S.AppButton>
+      </S.AppDetails>
 
-      <img src={PhoneImg} alt='Phone' />
-    </AppWrapper>
+      <img onLoad={() => wrapperRef.current !== null && setWrapperHeight(wrapperRef.current?.getBoundingClientRect().height)} ref={wrapperRef} src={PhoneImg} alt='Phone' />
+    </S.AppWrapper>
   )
 }
---
+++ src/app-section/styles/index.tsx
@@ -0,0 +1,76 @@
+import styled from 'styled-components'
+import { Button } from 'shared/styles'
+
+type AppWrapperProps = {
+  wrapperHeight: number
+}
+
+export const AppWrapper = styled.section<AppWrapperProps>`
+  padding: 3.375em 1em 0;
+  background-color: ${({ theme }) => theme.colors.primary};
+  display: flex;
+  flex-direction: column;
+
+  > img {
+    width: 100%;
+  }
+
+  @media (min-width: 800px) {
+    flex-direction: row-reverse;
+    align-items: center;
+    padding-top: 0;
+    gap: 2em;
+    height: ${({ wrapperHeight }) => `calc(${wrapperHeight}px - 80px)`};
+    margin-top: 8.188em;
+
+    > img {
+      width: 40%;
+      transform: translateY(-40px);
+    }
+  }
+`
+
+export const AppDetails = styled.div`
+  text-align: center;
+  max-width: 17.688em;
+  margin: 0 auto;
+  line-height: 1.5;
+
+  @media (min-width: 800px) {
+    text-align: start;
+    max-width: none;
+
+    > div {
+      display: flex;
+      gap: 1em;
+
+      img {
+        width: 50px;
+        height: 36px;
+        margin: auto 0;
+      }
+    }
+  }
+
+  h3 {
+    color: ${({ theme }) => theme.colors.secondary};
+    font-size: .875rem;
+  }
+
+  p {
+    color: ${({ theme }) => theme.colors.white};
+    font-size: .75rem;
+
+    @media (min-width: 800px) {
+      max-width: 30em;
+    }
+  }
+`
+
+export const AppButton = styled(Button)`
+  margin-top: 3em;
+
+  @media (min-width: 800px) {
+    margin: 0;
+  }
+`
